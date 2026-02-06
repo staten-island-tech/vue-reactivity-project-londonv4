5197bcea5456b6c98c9721ea7066fca23a690614
londonv4 <londonv4@nycstudents.net>
2026-02-06 12:31:13 -0500

    sav

---
+++ vue-project/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang= scoped>
+
+</style>
