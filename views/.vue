e275cbeea7188bbd29880d82cf3a735b0840fecf
londonv4 <londonv4@nycstudents.net>
2026-02-13 12:31:43 -0500

    friday

---
+++ views/.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { AnimalCard } from '@/components/xx.vue';
+const icecream = [
+flavour: {"Strawberry",
+"Chocolate"
+"Mint Chocolate Chip"
+}
+
+
+]
+
+</script>
+
+<style lang= scoped>
+
+</style>
