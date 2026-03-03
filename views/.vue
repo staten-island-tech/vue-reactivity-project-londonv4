0708e88aca32fcf26a0c2dd88cbb1080389a144f
londonv4 <londonv4@nycstudents.net>
2026-03-03 12:31:33 -0500

    tuesday

--- views/.vue
+++ views/.vue
@@ -1,23 +1,48 @@
 <template>
     <div>
-
+icecreamCard v-for="icecream in icecreams" :key="icecream.id">
+  {{ icecream }}
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import { AnimalCard } from '@/components/xx.vue';
-const icecream = [
-flavour: {"Strawberry",
-"Chocolate"
-"Mint Chocolate Chip"
-}
-
+const icecreams = ref([
+  { id: 1, flavor: 'Chocolate', price: 3.99 },
+  { id: 2, flavor: 'Strawberry', price: 3.99 },
+  { id: 3, flavor: 'Mint', price: 3.99 },
 
 ]
 
 </script>
 
 <style lang= scoped>
+div {
+  padding: 16px;
+}
 
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+.container {
+  display: flex;
+  width: 80vw;
+  margin: 20px auto;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.card {
+  border: 1px solid #ccc;
+  padding: 12px;
+  margin-bottom: 12px;
+  border-radius: 6px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
 </style>
