bf46c8edf5512ebd499d65dc04e56fcabb55a869
londonv4 <londonv4@nycstudents.net>
2026-03-05 12:31:42 -0500

    thursdayyyyy

---
+++ vue-project/src/components/icecreamCone.vue
@@ -0,0 +1,24 @@
+<<template>
+    <div>
+
+<h2>Your Icecream</h2>
+
+<img src="/src/assets/cone.png" >
+
+<ul>
+  <li v-for="item in scoops">
+    {{ item }}
+  </li>
+</ul>
+    </div>
+</template>
+
+<script>
+defineProps({
+  scoops: array
+</script>
+}
+)
+<style lang= scoped>
+
+</style>
